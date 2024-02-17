318d6b775e7e84c6b64db32c48c796b382aa9be3
Eric Sampaio <ericstudy97@gmail.com>
2024-02-17 09:51:22 -0300

    :sparkles: feat: Created the game logic.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,46 @@
-console.log('teste');
+let x = document.querySelector('.x');
+let o = document.querySelector('.o');
+let boxes = document.querySelectorAll('.box');
+let buttons = document.querySelectorAll('#buttons-container button');
+let messageContainer = document.querySelector('#message');
+let messageText = document.querySelector('#message  p');
+let secondPlayer;
+
+let player1 = 0;
+let player2 = 0;
+
+
+for(let i = 0; i < boxes.length; i++) {
+
+    boxes[i].addEventListener('click', function() {
+
+        let elemento =  checkPlayer(player1, player2)
+
+
+        if(this.childNodes.length === 0) {
+            let cloneElemento = elemento.cloneNode(true);
+
+            this.appendChild(cloneElemento);
+
+            if(player1 === player2) {
+                player1++;
+            } else {
+                player2++;
+            }
+        }
+
+    });
+
+}
+
+
+function checkPlayer(player1, player2) {
+
+    if(player1 == player2) {
+
+        elemento = x;
+    } else {
+
+        elemento = o;
+    }
+};
